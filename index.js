ef6407d04cc750f7d43fc58566a88edf3a2ffb93
John Oduor Otieno <johnoduor222@gmail.com>
2022-08-18 07:28:35 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === string.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(drivers, newString) {
+    return drivers.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(newString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(drivers, soughtName) {
+    return drivers.filter((record) => record.name === soughtName);
+  }
